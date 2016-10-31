2344cbe39f3fcc855e5fb1757001d4f6f6bc55c9
Dusty Wilson <dusty@linux.com>
2016-10-30 21:20:44 -0700

    Fixed build.bat Docker path

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 
-REM docker run -it --rm --hostname=oly-lic1-svr --mac-address=00:11:22:33:44:55 -v "%CD%\my.lic:/opt/my.lic:ro" -v "%CD%\lmgrd.log:/tmp/lmgrd.log:rw" scjalliance/flexnet %*
-echo docker run -it --rm --hostname=oly-lic1-svr --mac-address=be:05:6b:d1:fa:d0 -p 0.0.0.0:2080:2080 -p 0.0.0.0:27000:27000 -v c:/Users/dusty.wilson/Documents/nlm/my.lic:/opt/my.lic:ro scjalliance/flexnet %*
-docker run -it --rm --hostname=oly-lic1-svr --mac-address=be:05:6b:d1:fa:d0 -p 0.0.0.0:2080:2080 -p 0.0.0.0:27000:27000 -v c:/Users/dusty.wilson/Documents/nlm/my.lic:/opt/my.lic:ro scjalliance/flexnet %*
-REM docker run -it --rm --hostname=oly-lic1-svr --mac-address=00:11:22:33:44:55 -v "%CD%/my.lic:/opt/my.lic:ro" scjalliance/flexnet %*
-REM docker run -it --rm --hostname=oly-lic1-svr --mac-address=00:11:22:33:44:55 scjalliance/flexnet %*
+REM docker run -it --rm --hostname=oly-lic1-svr --mac-address=00:11:22:33:44:55 -v "%CD%\my.lic:/opt/my.lic:ro" -v "%CD%\lmgrd.log:/tmp/lmgrd.log:rw" scjalliance/adskflex %*
+echo docker run -it --rm --hostname=oly-lic1-svr --mac-address=be:05:6b:d1:fa:d0 -p 0.0.0.0:2080:2080 -p 0.0.0.0:27000:27000 -v c:/Users/dusty.wilson/Documents/nlm/my.lic:/opt/my.lic:ro scjalliance/adskflex %*
+docker run -it --rm --hostname=oly-lic1-svr --mac-address=be:05:6b:d1:fa:d0 -p 0.0.0.0:2080:2080 -p 0.0.0.0:27000:27000 -v c:/Users/dusty.wilson/Documents/nlm/my.lic:/opt/my.lic:ro scjalliance/adskflex %*
+REM docker run -it --rm --hostname=oly-lic1-svr --mac-address=00:11:22:33:44:55 -v "%CD%/my.lic:/opt/my.lic:ro" scjalliance/adskflex %*
+REM docker run -it --rm --hostname=oly-lic1-svr --mac-address=00:11:22:33:44:55 scjalliance/adskflex %*
